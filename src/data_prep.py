75fa08b5a689323861e796b32c4fd37e9a64fd7d
cjjin1 <cjamesjin7@gmail.com>
2025-07-19 13:27:54 -0400

    Modified data_prep.py and osm_roads.py on how road data is handled

--- src/data_prep.py
+++ src/data_prep.py
@@ -2,216 +2,118 @@
 # data_prep.py
 # Author: James Jin
 # unity ID: cjjin
-# Purpose: Prepares data for distance calculations. Filters exit points for NFS roads that connect to public roads.
-#          Projects all data to WGS1984. Snaps sawmills and exit points to roads dataset. Makes a raster from roads
-#          dataset.
-# Usage: <Workspace> <Feature Dataset> <Roads Dataset> <NFS Roads Shapefile> <sawmill shapefile> <harvest sites>
-#        <[optional] Boundary Shapefile>
+# Purpose: Handles and cleans harvest site data and sawmill data. This includes project, clipping, and removing
+#          unwanted features.
+#          Also handles road data by projecting and merging road data properly.
+# Usage: <workspace> <transportation dataset> <road data> <harvest site data> <sawmill data> <harvest site boundaries>
+#        <total study area boundaries> <spatial reference code>
 ########################################################################################################################
 
-import arcpy, sys, os
+import sys, arcpy, os
 
-#read in scratch directory
-scratch_dir = sys.argv[1]
-transportation_dataset = sys.argv[2]
-arcpy.env.workspace = scratch_dir
+def project_roads(road_data, transport_ds, sr):
+    """Simply projects roads data into new spatial reference"""
+    #project input roads into the transportation datasest
+    output_path = os.path.join(transport_ds, os.path.basename(road_data))
+    if not arcpy.Exists(output_path):
+        arcpy.management.Project(road_data, output_path, sr)
+
+def clean_harvest_site_data(harvest_site_data, sr, boundary):
+    """Projects and clips harvest site data. Also removes unwanted polygons that are too small."""
+    #project harvest site data and boundary
+    hs_proj = os.path.splitext(os.path.basename(harvest_site_data))[0]
+    if not arcpy.Exists(hs_proj):
+        arcpy.management.Project(harvest_site_data, hs_proj, sr)
+    bound_proj = os.path.basename(boundary).split(".")[0]
+    if not arcpy.Exists(os.path.basename(bound_proj)):
+        arcpy.management.Project(boundary, bound_proj, sr)
+
+    # extract the harvest site feature class inside the boundary
+    arcpy.management.MakeFeatureLayer(hs_proj, "harvest_layer")
+    arcpy.management.SelectLayerByLocation(
+        "harvest_layer", "WITHIN", bound_proj, selection_type="NEW_SELECTION"
+    )
+    hs_bound = "harvest_sites_bounded"
+    arcpy.management.CopyFeatures("harvest_layer", hs_bound)
+    arcpy.management.Delete("harvest_layer")
+
+    # remove all harvest sites under 60 square feet
+    arcpy.management.AddField(hs_bound, "area", "DOUBLE")
+    arcpy.management.CalculateGeometryAttributes(
+        hs_bound, [["area", "AREA_GEODESIC"]], area_unit="SQUARE_FEET_US"
+    )
+    uc = arcpy.da.UpdateCursor(hs_bound, ["area"])
+    for row in uc:
+        if row[0] < 60:
+            uc.deleteRow()
+    del uc, row
+
+def clean_sawmill_data(sawmill_data, sr, boundary):
+    """Projects and clips sawmill data. Removes closed and announced sawmills."""
+    #project sawmill data and boundary
+    sm_proj = os.path.splitext(os.path.basename(sawmill_data))[0]
+    if not arcpy.Exists(sm_proj):
+        arcpy.management.Project(sawmill_data, sm_proj, sr)
+    bound_proj = os.path.basename(boundary).split(".")[0]
+    if not arcpy.Exists(os.path.basename(bound_proj)):
+        arcpy.management.Project(boundary, bound_proj, sr)
+
+    #extract sawmill feature class inside the boundary
+    arcpy.management.MakeFeatureLayer(sawmill_data, "sawmill_layer")
+    arcpy.management.SelectLayerByLocation(
+        "sawmill_layer", "WITHIN", bound_proj, selection_type="NEW_SELECTION"
+    )
+    sm_bound = "sawmills_bounded"
+    arcpy.management.CopyFeatures("sawmill_layer", sm_bound)
+    arcpy.management.Delete("sawmill_layer")
+
+    # remove closed and announced sawmills from sawmill fc
+    uc = arcpy.da.UpdateCursor(sm_bound, ["Status"])
+    for row in uc:
+        if row[0] == "Closed":
+            uc.deleteRow()
+    del row, uc
+
+def create_network_dataset(transport_ds, roads_data):
+    """Cleans and merges the roads feature classes, then creates a network dataset out of the result"""
+    #set workspace to transportation feature dataset
+    arcpy.env.workspace = transport_ds
+
+    #create list of feature classes to merge, erasing from the larger roads dataset to remove duplicate roads
+    nfs_list = arcpy.ListFeatureClasses("*NFS*")
+    merge_list = []
+    erasing_fc = os.path.basename(roads_data)
+    for i, rd_nfs in enumerate(nfs_list):
+        arcpy.analysis.Erase(erasing_fc, rd_nfs, f"roads_erased_{i}")
+        erasing_fc = f"roads_erased_{i}"
+        merge_list.append(rd_nfs)
+    merge_list.append(erasing_fc)
+
+    #merge road feature classes
+    merged_roads = "merged_roads"
+    arcpy.management.Merge(merge_list, merged_roads)
+    arcpy.management.RepairGeometry(merged_roads)
+
+    #add and calculate distance field
+    arcpy.management.AddField(merged_roads, "distance", "DOUBLE")
+    arcpy.management.CalculateGeometryAttributes(
+        merged_roads, [["distance", "LENGTH_GEODESIC"]], "MILES_US"
+    )
+
+workspace = sys.argv[1]
+transport_dataset = sys.argv[2]
+roads = sys.argv[3]
+harvest_sites = sys.argv[4]
+sawmills = sys.argv[5]
+hs_boundary = sys.argv[6]
+sm_boundary = sys.argv[7]
+spat_ref = sys.argv[8]
+
+arcpy.env.workspace = workspace
 arcpy.env.overwriteOutput = True
 
-#read in shapefiles for public roads, NFS roads, sawmills, and state boundary
-roads = sys.argv[3]
-NFS_roads = sys.argv[4]
-sawmills = sys.argv[5]
-harvest_sites = sys.argv[6]
-
-#Project all feature classes
-SR = arcpy.SpatialReference(3404)
-if not arcpy.Exists(os.path.join(transportation_dataset, os.path.basename(roads))):
-    arcpy.Project_management(roads, os.path.join(transportation_dataset, os.path.basename(roads)), SR)
-if not arcpy.Exists(os.path.splitext(os.path.basename(NFS_roads))[0]):
-    arcpy.Project_management(NFS_roads, os.path.splitext(os.path.basename(NFS_roads))[0], SR)
-if not arcpy.Exists(os.path.splitext(os.path.basename(sawmills))[0]):
-    arcpy.Project_management(sawmills, os.path.splitext(os.path.basename(sawmills))[0], SR)
-if not arcpy.Exists(os.path.splitext(os.path.basename(harvest_sites))[0]):
-    arcpy.Project_management(harvest_sites, os.path.splitext(os.path.basename(harvest_sites))[0], SR)
-roads = os.path.join(transportation_dataset, os.path.basename(roads))
-sawmills = os.path.splitext(os.path.basename(sawmills))[0]
-NFS_roads = os.path.splitext(os.path.basename(NFS_roads))[0]
-harvest_sites = os.path.splitext(os.path.basename(harvest_sites))[0]
-
-#if a boundary shapefile is included
-if len(sys.argv) == 8:
-    boundary_shp = sys.argv[7]
-    if not arcpy.Exists(os.path.basename(boundary_shp).split(".")[0]):
-        arcpy.Project_management(boundary_shp, os.path.basename(boundary_shp).split(".")[0], SR)
-    boundary_shp = os.path.basename(boundary_shp).split(".")[0]
-    #Clip the NFS roads  to the state
-    arcpy.analysis.Clip(NFS_roads, boundary_shp, "NFS_bounded")
-    NFS_roads = "NFS_bounded"
-    #keep only the sawmills within Mississippi
-    arcpy.management.MakeFeatureLayer(sawmills, "sawmill_layer")
-    arcpy.management.SelectLayerByLocation(
-        "sawmill_layer", "WITHIN", boundary_shp, selection_type="NEW_SELECTION"
-    )
-    arcpy.management.CopyFeatures("sawmill_layer", "sawmills_bounded")
-    sawmills = "sawmills_bounded"
-    arcpy.management.Delete("sawmill_layer")
-    #extract the timber harvest feature class inside the boundary
-    arcpy.management.MakeFeatureLayer(harvest_sites, "harvest_layer")
-    arcpy.management.SelectLayerByLocation(
-        "harvest_layer", "WITHIN", boundary_shp, selection_type="NEW_SELECTION"
-    )
-    arcpy.management.CopyFeatures("harvest_layer", "harvest_sites_bounded")
-    arcpy.management.Delete("harvest_layer")
-    harvest_sites = "harvest_sites_bounded"
-
-#remove all harvest sites under 100 square feet
-arcpy.management.AddField(harvest_sites, "area", "DOUBLE")
-arcpy.management.CalculateGeometryAttributes(
-    harvest_sites, [["area", "AREA_GEODESIC"]], area_unit="SQUARE_FEET_US"
-)
-uc = arcpy.da.UpdateCursor(harvest_sites, ["area"])
-for row in uc:
-    if row[0] < 100:
-        uc.deleteRow()
-del uc, row
-
-#remove closed and announced sawmills from sawmill fc
-uc = arcpy.da.UpdateCursor(sawmills, ["Status"])
-for row in uc:
-    if row[0] == "Closed" or row[0] == "Announced":
-        uc.deleteRow()
-del row, uc
-
-#generate points along each NFS road
-points = "NFS_points"
-arcpy.management.GeneratePointsAlongLines(
-    NFS_roads, points, "PERCENTAGE", Percentage = 4, Include_End_Points = "END_POINTS"
-)
-
-#use Near on points to check for proximity to public roads
-#add a field to indicate if a point is near or not
-arcpy.analysis.Near(points, roads, search_radius="170 Feet")
-arcpy.management.AddField(points, "IS_NEAR", "SHORT")
-arcpy.management.CalculateField(
-    points, "IS_NEAR", "0 if !NEAR_DIST! == -1 else 1", "PYTHON3"
-)
-
-#mark every NFS road as a duplicate if more than 20 points is near a public road (> 80%)
-dup_dict = {}
-arcpy.management.AddField(NFS_roads, "DUPLICATE", "SHORT")
-sc = arcpy.da.SearchCursor(points, ["ORIG_FID", "IS_NEAR"])
-uc = arcpy.da.UpdateCursor(NFS_roads, ["OBJECTID","DUPLICATE"])
-for row in sc:
-    if not dup_dict.get(row[0]):
-        dup_dict[row[0]] = row[1]
-    else:
-        dup_dict[row[0]] += row[1]
-del row, sc
-
-for row in uc:
-    if dup_dict[row[0]] > 20:
-        row[1] = 1
-    else:
-        row[1] = 0
-    uc.updateRow(row)
-del row, uc
-
-#export all NFS roads that are not flagged as duplicates
-arcpy.conversion.ExportFeatures(NFS_roads, "NFS_cleaned", "DUPLICATE = 0")
-NFS_roads = "NFS_cleaned"
-
-#get points to snap the NFS roads to
-end_points = "end_points"
-arcpy.management.GeneratePointsAlongLines(
-    NFS_roads, end_points, Point_Placement="PERCENTAGE", Percentage=100, Include_End_Points="END_POINTS"
-)
-arcpy.analysis.Near(end_points, roads, search_radius="100 Feet", location="LOCATION", distance_unit="Miles")
-arcpy.CreateFeatureclass_management(
-    arcpy.env.workspace, "road_points", "POINT", spatial_reference=SR
-)
-sc = arcpy.da.SearchCursor(end_points, ["SHAPE@", "NEAR_X", "NEAR_Y"])
-ic = arcpy.da.InsertCursor("road_points", ["SHAPE@"])
-for shape, near_x, near_y in sc:
-    road_point = arcpy.PointGeometry(arcpy.Point(near_x, near_y), SR)
-    if near_x != -1 and near_y != -1:
-        ic.insertRow([road_point])
-del sc, ic, shape, near_x, near_y
-
-#remove NFS_roads that won't snap to any public road
-remove_count = 1
-while remove_count > 0:
-    remove_count = 0
-    arcpy.management.MakeFeatureLayer(end_points, "end_points_neg")
-    arcpy.management.SelectLayerByAttribute(
-        "end_points_neg", "NEW_SELECTION", "NEAR_DIST = -1"
-    )
-    arcpy.analysis.SpatialJoin(
-        target_features=end_points,
-        join_features=NFS_roads,
-        out_feature_class="spatial_join_output",
-        join_operation="JOIN_ONE_TO_ONE",
-        join_type="KEEP_ALL",
-        match_option="INTERSECT",
-        field_mapping="",
-        search_radius=None,
-        distance_field_name=""
-    )
-    arcpy.management.MakeFeatureLayer("spatial_join_output", "joined_lyr")
-    arcpy.management.SelectLayerByAttribute(
-        "joined_lyr", "NEW_SELECTION", '"Join_Count" >= 2'
-    )
-
-    orig_fid_dict = {}
-    sc = arcpy.da.SearchCursor("end_points_neg", ["ORIG_FID"])
-    for row in sc:
-        if orig_fid_dict.get(row[0]):
-            orig_fid_dict[row[0]] += 1
-        else:
-            orig_fid_dict[row[0]] = 1
-    del row, sc
-
-    sc = arcpy.da.SearchCursor("joined_lyr", ["ORIG_FID"])
-    for row in sc:
-        if orig_fid_dict.get(row[0]) and orig_fid_dict[row[0]] == 2:
-            orig_fid_dict[row[0]] -= 1
-    del row, sc
-
-    uc = arcpy.da.UpdateCursor(NFS_roads, ["OBJECTID"])
-    for row in uc:
-        if orig_fid_dict.get(row[0]) and orig_fid_dict[row[0]] == 2:
-            uc.deleteRow()
-            remove_count += 1
-    del row, uc
-    arcpy.management.Delete("joined_lyr")
-    arcpy.management.Delete("spatial_join_output")
-    arcpy.management.Delete("end_points_neg")
-
-#snap the cleaned NFS roads to the points on the public roads
-arcpy.edit.Snap(NFS_roads, [["road_points", "VERTEX", "100 Feet"]])
-
-#merge the two roads datasets
-output_roads = os.path.join(transportation_dataset, "all_roads")
-if arcpy.Exists(os.path.join(transportation_dataset, "streets_nd")):
-    arcpy.management.Delete(os.path.join(transportation_dataset, "streets_nd"))
-    arcpy.management.Delete(os.path.join(transportation_dataset, "streets_nd_Junctions"))
-arcpy.management.Merge([roads, NFS_roads], output_roads)
-
-#integreate the roads dataset then convert to line so that each line ends at an intersection
-arcpy.management.Integrate(output_roads, cluster_tolerance="0.5 Feet")
-arcpy.management.FeatureToLine(output_roads, os.path.join(transportation_dataset, "all_roads_fixed"))
-output_roads = os.path.join(transportation_dataset, "all_roads_fixed")
-
-#Add a distance field to the roads shapefile
-arcpy.management.AddField(output_roads, "distance", "DOUBLE")
-arcpy.management.CalculateGeometryAttributes(
-    output_roads, [["distance", "LENGTH_GEODESIC"]], "MILES_US"
-)
-
-#Create and build the network dataset using roads dataset
-# arcpy.na.CreateNetworkDataset(
-#     transportation_dataset,
-#     "streets_nd",
-#     [os.path.basename(output_roads)],
-#     "NO_ELEVATION"
-# )
-# arcpy.na.BuildNetwork(os.path.join(transportation_dataset, "streets_nd"))
+SR = arcpy.SpatialReference(int(spat_ref))
+project_roads(roads, transport_dataset, SR)
+clean_harvest_site_data(harvest_sites, SR, hs_boundary)
+clean_sawmill_data(sawmills, SR, sm_boundary)
+create_network_dataset(transport_dataset, roads)
--- src/osm_roads.py
+++ src/osm_roads.py
@@ -3,11 +3,11 @@
 # Author: James Jin
 # unity ID: cjjin
 # Purpose: Uses osmnx to retrieve roads data, can retrieve one specific type of road along with truck only roads
-# Usage: <output_roads_file> <output_nodes_files> <aoi> <road type[optional]>
-#        <output_roads_file> <output_nodes_files> <north> <south> <east> <west> <road type[optional]>
+# Usage: <output_roads_file> <output_nodes_files> <aoi> <aoi type(place or shapefile)>
 ########################################################################################################################
 
 import osmnx as ox
+import geopandas as gpd
 import sys, arcpy, os
 
 def export_to_arcgis(edges, file, layer):
@@ -24,42 +24,28 @@ def export_to_arcgis(edges, file, layer):
 arcpy.env.overwriteOutput = True
 
 #read in area of interest
-aoi = sys.argv[3]
-#create graph
-#if there are 6 input arguments, read in arguments as coordinates for bbox
-#otherwise, read in the first argument as the name of place for aoi
+aoi = sys.argv[2]
+aoi_type = sys.argv[3]
+#create graph with custom filter
 g_main = None
-g_single = None
-single_road_type = None
-
 cf = (
     '["highway"~"motorway|trunk|primary|secondary|tertiary|residential|unclassified|road"]'
 )
-if len(sys.argv) >= 7:
-    north, south, east, west = float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6])
-    g_main = ox.graph_from_bbox(
-        (north, south, east, west), custom_filter=cf, simplify=False, retain_all=True
-    )
-    if len(sys.argv) == 8:
-        single_road_type = sys.argv[7]
-        cf_single = f'["highway"="{single_road_type}"]'
-        g_single = ox.graph_from_bbox(
-            (north, south, east, west), custom_filter=cf_single, simplify=False, retain_all=True
-        )
-elif len(sys.argv) >= 4:
+#check what kind of input was read (shapefile or place name)
+if aoi_type == "aoi":
     g_main = ox.graph_from_place(aoi, custom_filter=cf, simplify=False, retain_all=True)
-    if len(sys.argv) == 5:
-        single_road_type = sys.argv[4]
-        cf_single = f'["highway"="{single_road_type}"]'
-        g_single = ox.graph_from_place(aoi, custom_filter=cf_single, simplify=False, retain_all=True)
+elif aoi_type == "shapefile":
+    gdf = gpd.read_file(aoi)
+    # Ensure CRS is EPSG:4326
+    if gdf.crs != "EPSG:4326":
+        gdf = gdf.to_crs("EPSG:4326")
+    polygon = gdf.geometry.iloc[0]
+    g_main = ox.graph_from_polygon(polygon, network_type="drive")
 
 #get nodes and edges from graph
 nodes_main, edges_main = ox.graph_to_gdfs(g_main)
 
 #save the roads to scratch folder as gpkg files, then export to feature class in File GDB
-temp_file = "E:/timber_project/scratch/temp_roads.gpkg"
-layer_name = "roads"
+temp_file = "C:/timber_project/scratch/temp_roads.gpkg"
+layer_name = "va_roads"
 export_to_arcgis(edges_main, temp_file, layer_name)
-if single_road_type:
-    nodes_single, edges_single = ox.graph_to_gdfs(g_single)
-    export_to_arcgis(edges_single, temp_file, f"{single_road_type}s")
