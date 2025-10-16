a9c22e8835c8770c8d28c6a620eb3f6587caae2b
cjjin1 <cjamesjin7@gmail.com>
2025-10-15 20:53:56 -0400

    removed unused code

--- src/distance_calculator.py
+++ src/distance_calculator.py
@@ -86,28 +86,6 @@ def calculate_distance_for_fc(fc_path):
     del row, sc
     return distance
 
-def euclidean_distance(hs_point, points, mill_type):
-    """Calculates the Euclidean distance between two points using near tool"""
-    arcpy.management.MakeFeatureLayer(points, "point_layer")
-    arcpy.management.SelectLayerByAttribute(
-        "point_layer", "NEW_SELECTION", f"Mill_Type = '{mill_type}'"
-    )
-    arcpy.analysis.Near(
-        hs_point, "point_layer","120 Miles" ,distance_unit = "Miles", method="PLANAR"
-    )
-    near_fid = 0
-    distance = 0
-    sc = arcpy.da.SearchCursor(hs_point, ["NEAR_FID","NEAR_DIST"])
-    for row in sc:
-        near_fid = row[0]
-        distance = row[1]
-        break
-    del row, sc
-    arcpy.management.Delete("point_layer")
-    if near_fid == -1 or distance == -1:
-        raise arcpy.ExecuteError("Euclidean distance: no harvest site within 120 miles of site")
-    return near_fid, distance
-
 def calculate_sl_distances(hv_sites, sawmill_data, sm_ts, op_dir):
     """Calculates straight line distance from every harvest site to every sawmill for every sawmill type"""
     csv_sl_out = os.path.join(op_dir, f"sl_distances_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}.csv")
@@ -160,87 +138,6 @@ def read_sl_distance_csv(sl_csv, di_dict):
     for row in sl_reader:
         di_dict[row[0]][row[1]] = (row[2], row[3])
     sl_in.close()
-
-def calculate_road_distances(di_dict, ppt, op_dir, hv_sites, sm_data, nw_ds, kop):
-    """Calculates circuity factor for each sawmill type, can accommodate different sample sizes per sawmill type"""
-    # select m number of random ids from each of the type's lists
-    # calculate the road distance for each of the ids and sawmill id pairs
-    # add to rd_list and ed_list, as well as write out to a csv file
-    arcpy.AddMessage("Starting Road Distance Calculations")
-    for sm_type in di_dict:
-
-        # output file for distance results so the full script doesn't have to run every time
-        csv_out = os.path.join(op_dir, f"{sm_type[:3]}_distance.csv")
-        output_file = open(csv_out, "w+", newline="\n")
-        output_writer = csv.writer(output_file)
-
-        oid_list = list(di_dict[sm_type].keys())
-        sample_size = ppt
-        if isinstance(ppt, dict):
-            sample_size = int(ppt[sm_type])
-        rand_id_list = random.sample(oid_list, sample_size)
-        remaining_ids = list(set(oid_list) - set(rand_id_list))
-        count = 1
-        for rand_id in rand_id_list:
-            road_dist = None
-            id_to_try = rand_id
-            while not road_dist:
-                try:
-                    time.sleep(0.5)
-                    gc.collect()
-                    arcpy.management.MakeFeatureLayer(hv_sites, f"harvest_site_{id_to_try}")
-                    arcpy.management.MakeFeatureLayer(sm_data, f"sawmill_layer_{id_to_try}")
-                    arcpy.management.SelectLayerByAttribute(
-                        f"harvest_site_{id_to_try}",
-                        "NEW_SELECTION",
-                        f"OBJECTID = {id_to_try}"
-                    )
-                    arcpy.management.SelectLayerByAttribute(
-                        f"sawmill_layer_{id_to_try}",
-                        "NEW_SELECTION",
-                        f"OBJECTID = {di_dict[sm_type][id_to_try][0]}"
-                    )
-                    out_path = os.path.join(arcpy.env.workspace, f"path_{sm_type[:3]}_{id_to_try}")
-                    road_dist = calculate_route_distance(
-                        f"harvest_site_{id_to_try}", nw_ds, f"sawmill_layer_{id_to_try}", out_path
-                    )
-                    time.sleep(0.25)
-                    gc.collect()
-                    if not kop:
-                        arcpy.management.Delete(out_path)
-                    if road_dist == 0:
-                        raise arcpy.ExecuteError("Solve resulted in failure")
-                    output_writer.writerow(
-                        [id_to_try,
-                         di_dict[sm_type][id_to_try][0],
-                         di_dict[sm_type][id_to_try][1],
-                         road_dist]
-                    )
-                except arcpy.ExecuteError as e:
-                    arcpy.AddWarning(f"{sm_type}:{id_to_try},{di_dict[sm_type][id_to_try][0]} failed: {str(e)}")
-                    if remaining_ids:
-                        id_to_try = random.choice(remaining_ids)
-                        remaining_ids.remove(id_to_try)
-                        arcpy.AddWarning(f"Attempting new ID: {id_to_try}, {di_dict[sm_type][id_to_try][0]}")
-                        road_dist = None
-                    else:
-                        arcpy.AddWarning("No more IDs to try, skipping this distance calculation")
-                        break
-                finally:
-                    # delete temporary layers, feature classes, and solvers
-                    arcpy.management.Delete(f"harvest_site_{id_to_try}")
-                    arcpy.management.Delete(f"sawmill_layer_{id_to_try}")
-                    for name in arcpy.ListDatasets("*Solver*"):
-                        arcpy.management.Delete(name)
-                    time.sleep(0.25)
-                    gc.collect()
-                    arcpy.management.ClearWorkspaceCache()
-            if id_to_try != rand_id:
-                arcpy.AddMessage(f"New ID ({id_to_try}) successful")
-            arcpy.AddMessage(f"Site {count}: Harvest Site {id_to_try} calculated.")
-            count += 1
-
-        output_file.close()
 
 def calculate_circuity_factor_from_csv(rd_csv, output_name, op_dir, sawmill_type, pdf_file):
     """Reads in road distance csv created by the road distance calculation functions. Returns coefficent
