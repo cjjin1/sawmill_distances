81b2fe2d20117c24b6ff92fcf9f8aee75183d053
cjjin1 <cjamesjin7@gmail.com>
2025-10-23 19:03:53 -0400

    reworked create_boundaries.py to be more simple

--- src/prep/create_boundaries.py
+++ src/prep/create_boundaries.py
@@ -5,68 +5,33 @@
 # Purpose: Creates a boundary shapefile for osm_roads_planet.py to use. This shapefile is a 125-mile buffer around
 #          all park boundaries within the designated physiographic region. The output shapefile will go into the
 #          directory the physiographic boundary shapefile is in. All other intermediate files will be deleted.
-# Usage: <park boundaries (ranger_districts.shp)> <physiographic shapefile/fc> <spatial reference: epsg code>
-#        <optional: region_name>
-#        Important note: although region name is optional, if no region name is selected, the output files, including
-#        intermediate files, will have default names. The output files may be overwritten if they already exist inside
-#        the directory the physiographic region shapefile is in.
+# Usage: <park boundaries (ranger_districts.shp)> <physiographic shapefile/fc> <output name>
+#        Important note: the output name can only be the basename + extension (no extension if in File GDB), although a
+#        full file path is a valid output name.
 ########################################################################################################################
 
 import sys,os,arcpy
-
-from data_prep import DataPrep
 
 arcpy.env.overwriteOutput = True
 
 #get inputs
 park = sys.argv[1]
 physio = sys.argv[2]
-sr = sys.argv[3]
-region_name = None
-if len(sys.argv) == 5:
-    region_name = sys.argv[4]
+output = sys.argv[3]
 
 #set workspace to physio's directory
 arcpy.env.workspace = os.path.dirname(physio)
 
-#construct DataPrep object
-data_prepper = DataPrep(
-    park_boundaries=park,
-    physio_boundary=physio,
-    spat_ref=sr,
-)
+#clip the ranger districts by the physiographic region
+arcpy.analysis.Clip(park, physio, "temp_park_bd")
 
-#check if region name has been given
-#call create_boundary_fcs()
-#delete intermediate files
-#project back to NAD 83
-if region_name:
-    data_prepper.create_boundary_fcs(
-        new_physio=f"proj_physio_{region_name}.shp",
-        new_park_boundaries=f"park_boundaries_{region_name}.shp",
-        new_sm_boundaries=f"sm_boundaries_temp_{region_name}.shp"
+#buffer the temp park boundary by 125 miles
+arcpy.analysis.Buffer(
+    "temp_park_bd",
+    output,
+    buffer_distance_or_field="125 Miles",
+    dissolve_option="ALL"
     )
-    arcpy.management.Delete(f"park_boundaries_{region_name}.shp")
-    arcpy.management.Delete(f"proj_physio_{region_name}.shp")
 
-    arcpy.management.Project(
-        f"sm_boundaries_temp_{region_name}.shp",
-        f"sm_boundaries_{region_name}.shp",
-        arcpy.SpatialReference(4269)
-    )
-    arcpy.management.Delete(f"sm_boundaries_temp_{region_name}.shp")
-else:
-    data_prepper.create_boundary_fcs(
-        new_physio="proj_physio.shp",
-        new_park_boundaries="park_boundaries.shp",
-        new_sm_boundaries="sm_boundaries_temp.shp"
-    )
-    arcpy.management.Delete("park_boundaries.shp")
-    arcpy.management.Delete("proj_physio.shp")
-
-    arcpy.management.Project(
-        f"sm_boundaries_temp.shp",
-        f"sm_boundaries.shp",
-        arcpy.SpatialReference(4269)
-    )
-    arcpy.management.Delete(f"sm_boundaries_temp.shp")
+#delete temp files
+arcpy.management.Delete("temp_park_bd")
