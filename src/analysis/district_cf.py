73f3760315be8cd9f23b670ef6b34513fc9b32fb
cjjin1 <cjamesjin7@gmail.com>
2025-12-12 22:49:07 -0500

    added code to write out total cfs

--- src/analysis/district_cf.py
+++ src/analysis/district_cf.py
@@ -15,6 +15,7 @@ class DistrictCF:
     def __init__(self, csv_dir, write_out=False):
         self.csv_dir = csv_dir
         self.write_out = write_out
+        #sm_type: {district: [(hs_oid, sm_oid, ed, rd), (hs_oid, sm_oid, ed, rd), ...]}
         self.district_dict = {
             "Lumber/Solid Wood": {},
             "Pellet": {},
@@ -23,6 +24,7 @@ class DistrictCF:
             "Composite Panel/Engineered Wood Product": {},
             "Plywood/Veneer": {}
         }
+        # sm_type: {district: (cf, sample size)}
         self.district_results_dict = {
             "Lumber/Solid Wood": {},
             "Pellet": {},
@@ -80,15 +82,53 @@ class DistrictCF:
                     ])
             output_file.close()
 
+    def write_total_cf_output(self):
+        """Writes out the total circuity factor results for each ranger district into csv file"""
+        #district: [(ed_list, rd_list), (ed_list, rd_list), ...]
+        by_districts = {}
+        for sm_type in self.district_dict:
+            for district in self.district_dict[sm_type]:
+                compiled_list = self.district_dict[sm_type][district]
+                ed_list = [float(entry[2]) for entry in compiled_list]
+                rd_list = [float(entry[3]) for entry in compiled_list]
+                if not by_districts.get(district):
+                    by_districts[district] = []
+                by_districts[district].append((ed_list, rd_list))
+
+        csv_out = os.path.join(self.csv_dir, f"total_cf_by_district.csv")
+        output_file = open(csv_out, "w", newline="\n")
+        out_writer = csv.writer(output_file)
+        for district in by_districts:
+            ed_rd_lists = by_districts[district]
+
+            ed_list = []
+            rd_list = []
+
+            for pair in ed_rd_lists:
+                ed_list += pair[0]
+                rd_list += pair[1]
+
+            road_distance = np.array(rd_list)
+            euclidean_distance = np.array(ed_list)
+
+            df = pd.DataFrame({
+                'sl': euclidean_distance,
+                'rd': road_distance
+            })
+            model = sm.OLS(df['rd'], df[['sl']]).fit()
+            result = model.params['sl']
+            out_writer.writerow([district, result, len(ed_list)])
+
     def process(self):
         self.compile_data()
         self.build_results_dict()
         if self.write_out:
             self.write_csv_output()
+            self.write_total_cf_output()
 
 def main():
     try:
-        dg = DistrictCF(sys.argv[1])
+        dg = DistrictCF(sys.argv[1], True)
         dg.process()
     except IndexError:
         print("Provide a directory to retrieve all necessary data and ranger district feature class.")
